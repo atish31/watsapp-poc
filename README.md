0ed49b4ebc1625871ce4ab0e5f0ada7d30d3d2d5
atish <atish.raina91@gmail.com>
2023-06-17 18:27:59 +0530

    use wbm package to send watsapp message in bulk

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
+# disclaimer:
+this is just a POC and not verified for use in production. Kindly proceed with due deligence.
+
 # watsapp-poc
 poc to send multiple watsapp messages
+
+# index.js:
+phone numbers @ line 4
+message @ line 5
+
+#how to run:
+npm i
+node index.js
+scan qr code with whatsapp and messge will be sent
--- index.js
+++ index.js
@@ -1,67 +1,8 @@
-const { Client } = require('whatsapp-web.js');
+const wbm = require('wbm');
 
-// Create a new client instance
-const client = new Client();
-
-// Event fired when the client is ready
-client.on('ready', () => {
-  console.log('Client is ready!');
-
-  // Replace the array of phone numbers with the desired recipients in international format
-  const phoneNumbers = ['9620662262', '9643731032'];
-
-  // Send messages to each recipient
-  phoneNumbers.forEach((phoneNumber) => {
-    client.sendMessage(phoneNumber, 'test message for watsapp code integration; please ignore')
-      .then(() => {
-        console.log(`Message sent successfully to ${phoneNumber}`);
-      })
-      .catch((error) => {
-        console.error(`Error sending message to ${phoneNumber}:`, error);
-      });
-  });
-
-  client.destroy(); // Close the client connection after sending all the messages
-});
-
-// Event fired when a QR code is received and needs to be scanned
-client.on('qr', (qrCode) => {
-  // Display the QR code for scanning
-  console.log('Scan the QR code below:');
-  console.log(qrCode);
-  getQRimage(qrCode);
-});
-
-// Event fired when authentication is successful
-client.on('authenticated', (session) => {
-  console.log('Authenticated!');
-  console.log('Saving session data...');
-  // Save the session data for reuse
-  // You can serialize and store the session object in a file or database
-  // This way, you won't need to authenticate again on future runs
-  // For simplicity, we'll just print the session data in this example
-  console.log(session);
-});
-
-// Initialize the client
-client.initialize();
-
-
-function getQRimage(code) {
-  console.log('here');
-  const qr = require('qrcode');
-const fs = require('fs');
-
-// Replace 'Hello, World!' with the string of characters you want to encode as a QR code
-const text = 'Hello, World!';
-
-// Generate the QR code
-qr.toFile('qrcode.png', code, (err, _) => {
-  if (err) {
-    console.error('Error generating QR code:', err);
-    return;
-  }
-
-  console.log('QR code generated successfully!');
-});
-}
+wbm.start().then(async () => {
+    const phones = ['96********', '96********'];
+    const message = 'test for bulk whatsapp message sending; please ignore';
+    await wbm.send(phones, message);
+    await wbm.end();
+}).catch(err => console.log(err));
